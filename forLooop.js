3ecebf0853ebe02e0fffe1210ad672a4f2a90883
Alejandra Olivo <alejandraolivo.student@careerdevs.com>
2018-10-16 00:06:05 +0000

    loop practice with integers

---
+++ forLooop.js
@@ -0,0 +1,7 @@
+var evensOdds = [[2,4,6,8], [1,3,5,7,9]];
+
+for (var i = 0; i < evensOdds.length; i++) {
+    for (var x = 0; x < evensOdds[i].length; x++) {
+    console.log(evensOdds[i][x]);
+    }
+}
