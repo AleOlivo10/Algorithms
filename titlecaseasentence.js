ae621383e2c47044e5811545659ccd4880e69a10
Alejandra Olivo <alejandraolivo.student@careerdevs.com>
2018-11-29 02:16:11 +0000

    stuck here

--- titlecaseasentence.js
+++ titlecaseasentence.js
@@ -1,17 +1,11 @@
-String.prototype.capitalize = function() {
-     this.charAt(0).toUpperCase() + this.slice(1);
-} 
-
-
-"hello world".capitalize();  =>  "Hello world" 
-var word = "hello World"
 
 var word = "hello world"
 function capitalizeFirstLetter(word) {
     return word.charAt(0).toUpperCase() + word.slice(1);
 }
 
-function titleCase("hello world") {
-    console.log("hello world".charAt(0).toUpperCase() + "hello world".slice(1));
+function titleCase(str) {
+    var str = str.charAt(0).toUpperCase + str.slice(1) + str.split(" ");
+    return str;
 }
-titleCase("hello world")
+ titleCase("hello world");
