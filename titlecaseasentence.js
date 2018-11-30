a489b614ae371f82c926faa163d091c31b99f4b9
Alejandra Olivo <alejandraolivo.student@careerdevs.com>
2018-11-30 00:31:38 +0000

    still in progress

--- titlecaseasentence.js
+++ titlecaseasentence.js
@@ -1,11 +1,25 @@
-
-var word = "hello world"
-function capitalizeFirstLetter(word) {
-    return word.charAt(0).toUpperCase() + word.slice(1);
-}
-
 function titleCase(str) {
-    str = str.charAt(0).toUpperCase() + str.slice(1);
+    var title = ("")
+    str = str.replace(str, str.charAt(0).toUpperCase() + str.slice(1));
+    for (i = 0; i > str.replace(str, str.charAt(0).toUpperCase() + str.slice(1)).length; i++) {
     return str;
 }
- titleCase("hello world");
+ titleCase("i'm a little tea pot");
+ 
+ 
+ 
+ function titleCase(str) {
+     var lower = str.toLowerCase();
+     var space = lower.split(" ");
+     for (var i=0; i > space.length; i++) {
+         
+     }
+     return str;
+ }
+ titleCase("HELLO WORLD");
+ 
+ 
+ 
+ 
+ 
+ 
